6ed894fce0f6bc01a2212f6aacb6bab39f7ca247
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-07-14 10:16:32 -0400

    speed context creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import { PathfindingProvider } from "./context/PathfindingContext";
+import { SpeedProvider } from "./context/SpeedContext";
+import { TileProvider } from "./context/TileContext";
 
 function App() {
-  <PathfindingProvider>
-    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-  </PathfindingProvider>;
+  return (
+    <PathfindingProvider>
+      <TileProvider>
+        <SpeedProvider>
+          <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        </SpeedProvider>
+      </TileProvider>
+    </PathfindingProvider>
+  );
 }
 
 export default App;
---
+++ src/context/SpeedContext.tsx
@@ -0,0 +1,20 @@
+import { ReactNode, createContext, useState } from "react";
+import { SpeedType } from "../utils/types";
+
+export interface SpeedContextInterface {
+  speed: SpeedType;
+  setSpeed: (speed: SpeedType) => void;
+}
+
+export const SpeedContext = createContext<SpeedContextInterface | undefined>(
+  undefined
+);
+
+export const SpeedProvider = ({ children }: { children: ReactNode }) => {
+  const [speed, setSpeed] = useState<SpeedType>(0.5);
+  return (
+    <SpeedContext.Provider value={{ speed, setSpeed }}>
+      {children}
+    </SpeedContext.Provider>
+  );
+};
