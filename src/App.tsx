71dae50187dba29f5b1813e0c892ff9efc004b40
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-07-14 09:45:46 -0400

    pathfinding context

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
+import { PathfindingProvider } from "./context/PathfindingContext";
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  <PathfindingProvider>
+    return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  </PathfindingProvider>;
 }
 
 export default App;
---
+++ src/utils/types.ts
@@ -0,0 +1,17 @@
+export type AlgorithmType = "DIJKSTRA" | "A_STAR" | "BFS" | "DFS";
+
+export type MazeType = "NONE" | "BINARY_TREE" | "RECURSIVE_DIVISION";
+
+export type TileType = {
+  row: number;
+  col: number;
+  isEnd: boolean;
+  isWall: boolean;
+  isPath: boolean;
+  distance: number;
+  isStart: boolean;
+  isTraversed: boolean;
+  parent: TileType | null;
+};
+
+export type GridType = TileType[][];
